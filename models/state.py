59fc7bb9e8c20ac45da2d6f305bacfeca997a50d
mehari123 <88460961+mehari123@users.noreply.github.com>
2022-11-14 10:48:57 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -6,6 +6,5 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """Implements the State model for any state object"""
     name = ""
-    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
